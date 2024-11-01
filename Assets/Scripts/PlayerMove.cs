d37491459728c1564301aa67617fe0ff02d27efb
borichaa <bumgyuseo101@gmail.com>
2024-11-01 12:24:59 +0900

    Changes to Movement

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,16 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField]float speed = 0.5f;
+    [SerializeField]float maxSpeed = 0.5f;
+    float accel;
+    float deccel;
+
+    [SerializeField] int framesToAccel = 20;
+    [SerializeField] int framesToDeccel = 10; 
+
+    [SerializeField]float velocityX = 0f;
     Rigidbody2D rb;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        accel = maxSpeed/framesToAccel;
+        deccel = maxSpeed/framesToDeccel;
     }
 
     // Update is called once per frame
@@ -18,14 +30,10 @@ public class PlayerMove : MonoBehaviour
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        transform.position += Vector3.right * h * speed * Time.deltaTime;
+        
+        Move(h, v);
 
-        Vector3 currentScale = transform.localScale;
-        if((h < 0 && currentScale.x > 0) || (h > 0 && currentScale.x < 0)){
-            currentScale.x *= -1;
-        }
-        transform.localScale = currentScale;
-
+        Animate(h , v);
         Ladder(v);
     }
 
@@ -49,8 +57,44 @@ public class PlayerMove : MonoBehaviour
 
     void Ladder(float v) {
         if (gameObject.layer == 8) {
-            transform.Translate(0, v * speed * Time.deltaTime, 0);
+            transform.Translate(0, v * maxSpeed * Time.deltaTime, 0);
         }
+    }
+
+    void Move(float horizontalInput, float verticalInput) {
+        float calculatedVelocity = velocityX;
+
+        //holding an input
+        if (horizontalInput != 0) {
+            // going right, accelerating to the right
+            if (horizontalInput > 0 && velocityX >= 0) calculatedVelocity += accel;
+            // going right, deccelerating to the left
+            if (horizontalInput < 0 && velocityX > 0) calculatedVelocity -= deccel;
+            //going left, accelerating to the left
+            if (horizontalInput < 0 && velocityX <= 0) calculatedVelocity -= accel;
+            //going left, deccelerating to the right
+            if (horizontalInput > 0 && velocityX < 0) calculatedVelocity += deccel;
+        }
+        // not holding an input
+        else {
+            calculatedVelocity -= deccel * Math.Sign(calculatedVelocity);
+        }
+
+        // set new velocity
+        if (calculatedVelocity >= 0) velocityX = Math.Min(calculatedVelocity, maxSpeed);
+        else velocityX = Math.Max(calculatedVelocity, -maxSpeed);
+        
+
+        Vector3 velocity = Vector3.right * velocityX * Time.deltaTime;
+        transform.position += velocity;
+    }
+
+    void Animate(float horizontalInput, float verticalInput) {
+        Vector3 currentScale = transform.localScale;
+        if((horizontalInput < 0 && currentScale.x > 0) || (horizontalInput > 0 && currentScale.x < 0)){
+            currentScale.x *= -1;
+        }
+        transform.localScale = currentScale;
     }
 
     
