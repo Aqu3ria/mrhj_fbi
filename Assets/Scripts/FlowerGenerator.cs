ed7523ba63a5661e9704c3b3f5328aa82ce8c8aa
MorningSnow <lunrarari0301@gmail.com>
2024-11-03 06:22:46 +0900

    Flower spawning on a random position of the platform

--- Assets/Scripts/FlowerGenerator.cs
+++ Assets/Scripts/FlowerGenerator.cs
@@ -3,54 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
 public class FlowerGenerator : MonoBehaviour
-{
-    
+{   
     [SerializeField]public int spawnInterval = 5;
-    int count = 3;
     private BoxCollider2D area;  
     private List<GameObject> BigFlowerList = new List<GameObject>();		
 
     void Start()
     {
-        area = GetComponent<BoxCollider2D>();
         StartCoroutine("Spawn", spawnInterval);
+        BigFlowerList.Clear();
     }
 
+    IEnumerator Spawn(float delayTime)
 
-    
-    private IEnumerator Spawn(float delayTime)
     {
         GameObject BigFlower = GameObject.Find("Flower");
+        GameObject [] SpawningPlatforms = GameObject.FindGameObjectsWithTag("FlowerSpawnable_Platform");
+        GameObject SpawningPlatform = SpawningPlatforms[Random.Range(0, SpawningPlatforms.Length)];
+        print(SpawningPlatform);
 
-        for (int i = 0; i < count; i++) 
-        {
-            Vector3 spawnPos = GetRandomPosition(); 
+        area = SpawningPlatform.GetComponent<BoxCollider2D>();
+        print(area);
 
-            yield return new WaitForSeconds(delayTime);
-
-            GameObject instance = Instantiate(BigFlower, spawnPos, Quaternion.identity);
-            BigFlowerList.Add(instance);
-        }
-        area.enabled = false;
+        Vector3 spawnPos = GetRandomPosition();
+        print(spawnPos);
+        GameObject instance = Instantiate(BigFlower, spawnPos, Quaternion.identity);
+        BigFlowerList.Add(instance);
         yield return new WaitForSeconds(delayTime);
 
-        for (int i = 0; i < count; i++)
-            Destroy(BigFlowerList[i].gameObject);
-
-        BigFlowerList.Clear();
-        area.enabled = true;
         StartCoroutine("Spawn", spawnInterval);
     }
+
+    
 
     private Vector2 GetRandomPosition()
     {
-        Vector2 basePosition = transform.position; 
+        Vector2 basePosition = area.transform.position; 
         Vector2 size = area.size;                  
         float posX = basePosition.x + Random.Range(-size.x / 2f, size.x / 2f);
 
-        float posY = basePosition.y;
+        float posY = basePosition.y + 0.5f;
 
         Vector2 spawnPos = new Vector2(posX, posY);
 
