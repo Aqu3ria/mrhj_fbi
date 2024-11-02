ed2407d5d5e65ef500019eadb9ea5e26f0ddb5c1
MorningSnow <lunrarari0301@gmail.com>
2024-11-03 03:30:19 +0900

    change Flowergenerator

--- Assets/Scripts/FlowerGenerator.cs
+++ Assets/Scripts/FlowerGenerator.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class FlowerGenerator : MonoBehaviour
 {
-    [SerializeField]public GameObject BigFlower;
-
+    
+    [SerializeField]public int spawnInterval = 5;
     int count = 3;
     private BoxCollider2D area;  
     private List<GameObject> BigFlowerList = new List<GameObject>();		
@@ -13,12 +15,15 @@ public class FlowerGenerator : MonoBehaviour
     void Start()
     {
         area = GetComponent<BoxCollider2D>();
-        StartCoroutine("Spawn", 5);
+        StartCoroutine("Spawn", spawnInterval);
     }
+
 
     
     private IEnumerator Spawn(float delayTime)
     {
+        GameObject BigFlower = GameObject.Find("Flower");
+
         for (int i = 0; i < count; i++) 
         {
             Vector3 spawnPos = GetRandomPosition(); 
@@ -36,15 +41,16 @@ public class FlowerGenerator : MonoBehaviour
 
         BigFlowerList.Clear();
         area.enabled = true;
-        StartCoroutine("Spawn", 5);
+        StartCoroutine("Spawn", spawnInterval);
     }
 
     private Vector2 GetRandomPosition()
     {
         Vector2 basePosition = transform.position; 
         Vector2 size = area.size;                  
-        float posX = basePosition.x + Random.Range(-size.x / 2f, size.x*10);
-        float posY = basePosition.y + 1;
+        float posX = basePosition.x + Random.Range(-size.x / 2f, size.x / 2f);
+
+        float posY = basePosition.y;
 
         Vector2 spawnPos = new Vector2(posX, posY);
 
