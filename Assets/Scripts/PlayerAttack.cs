2a0ce05c7ef668f99b5ee9b5ba3527983f7875d5
hajePC53 <>
2024-11-17 13:49:32 +0900

    added pickup and release weapon functionality

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -7,13 +7,58 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] Transform enemy;
     [SerializeField] Transform transform;
 
+    [SerializeField]
+    private Transform grabPoint;
+
+    [SerializeField]
+    private Transform rayPoint;
+    [SerializeField]
+    private float rayDistance;
+
+    private GameObject grabbedObject;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) // 스페이스바를 눌러 공격
-        {
+        {   
+            if (grabbedObject == null) {
+                Debug.Log("pickup");
+                GrabWeapon(); 
+            }
+            
             Debug.Log("Attack");
             AttackEnemy();
+            
         }
+
+        if (Input.GetKeyDown(KeyCode.Q) && grabbedObject != null) {
+            ReleaseWeapon();
+        }
+
+    }
+
+    void GrabWeapon() {
+        RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, transform.right * Mathf.Sign(transform.localScale.x), rayDistance);
+
+        if (hitInfo.collider != null && hitInfo.collider.gameObject.tag == "Flower") 
+        {
+            // grab object
+            grabbedObject = hitInfo.collider.gameObject;
+            grabbedObject.transform.position = grabPoint.position;
+            grabbedObject.transform.SetParent(transform);
+            
+        }
+
+        Debug.DrawRay(rayPoint.position, transform.right * rayDistance);
+    }
+
+    void ReleaseWeapon() {
+        // release object
+        grabbedObject.transform.SetParent(null);
+        // TODO: implement delete animation
+        Destroy(grabbedObject);
+        grabbedObject = null;
+
     }
 
     void AttackEnemy()
